fce0462705f19b5c884053877a6b989c517f96eb
Ubuntu <ubuntu@ip-10-0-186-116.ec2.internal>
2024-09-27 18:11:10 +0000

    cl100k pat_str

--- src/openai_public.rs
+++ src/openai_public.rs
@@ -158,8 +158,9 @@ impl EncodingFactory {
     //     r"\s+(?!\S)",
     //     r"\s+",
     // ].join("|");
+    let pat_str = r"(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]+|\s+(?!\S)|\s+",
 
-    Encoding::new("codestral", "", mergeable_ranks, special_tokens, None)
+    Encoding::new("codestral", pat_str, mergeable_ranks, special_tokens, None)
       .map_err(|e| EncodingFactoryError::UnableToCreateEncoding(e.to_string()))
   }
 
