615bf1fec76e9b7974befb9e106bfe4de9ebd487
Aman Karmani <aman@tmm1.net>
2024-10-16 17:06:07 -0700

    remove Arc<> for encoder hashmap and use directly (#12)
    partial revert of #11
    
    was seeing weird perf regression in benchmarks
    
    rust borrow checker makes it really hard for two things to own the same
    data, so take the simple way out by letting CoreBPE own mergeable_ranks

--- src/corebpe.rs
+++ src/corebpe.rs
@@ -166,7 +166,7 @@ const MAX_NUM_THREADS: usize = 8;
 
 #[derive(Debug)]
 pub struct CoreBPE {
-    encoder: Arc<HashMap<Vec<u8>, usize>>,
+    pub encoder: HashMap<Vec<u8>, usize>,
     special_tokens_encoder: HashMap<String, usize>,
     decoder: HashMap<usize, Vec<u8>>,
     special_tokens_decoder: HashMap<usize, Vec<u8>>,
@@ -429,7 +429,7 @@ impl CoreBPE {
 
 impl CoreBPE {
     pub fn new(
-        encoder: Arc<HashMap<Vec<u8>, usize>>,
+        encoder: HashMap<Vec<u8>, usize>,
         special_tokens_encoder: HashMap<String, usize>,
         pattern: &str,
     ) -> Result<Self, fancy_regex::Error> {
